7e8173eef503be0e21c1d44893dfa68f1a600522
turnerbp <turnerbp@fullsail.edu>
2013-09-10 12:23:37 -0400

    Added the Pizza Part One Exercise.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -13,3 +13,19 @@ var actualAge = sparkysAge / dogYears;
 // Result of Sparkys age Divided by Dog Years
 // print out the output of the Dogs Age
 console.log ("The Dogs age is " + actualAge);
+
+
+// pizza part 1
+
+//number of slices per pizza
+var pSlice = 8;
+//number of people at the party
+var numPeople = 27;
+//number of slices per person
+var perPerson = 2;
+//number of pizzas ordered
+var pizzOrdered = perPerson * numPeople / pSlice;
+//total pizza ordered
+//print out the output "I ordered a total of "
+console.log ("I ordered a total of " + pizzOrdered + " Pizzas");
+
