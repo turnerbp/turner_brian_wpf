b724576e2d85075d0f1a85ab098cb580d97226e5
turnerbp <turnerbp@fullsail.edu>
2013-09-10 11:51:03 -0400

    The js that calculates the Dog Years problem

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Brian Turner
+ * 9/10/13
+ * Expressions Worksheet
+ */
+
+// Sparkys Age
+var sparkysAge = 84;
+// Dogs age 7 times faster than humans
+var dogYears = 7;
+// Sparkys age divided by Dog Years
+var actualAge = sparkysAge / dogYears;
+// Result of Sparkys age Divided by Dog Years
+console.log (actualAge);
