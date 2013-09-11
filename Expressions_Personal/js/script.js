f2c5337e7488246f1f2d1f317f338602bddfbe74
turnerbp <turnerbp@fullsail.edu>
2013-09-11 11:18:19 -0400

    Added prompt asking how many hours do you practice at one time.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -8,7 +8,9 @@
 
 //This is the amount of times I practice my dancing
 var dancePractice = prompt("How many times per week do you practice your dancing?")
+//This is the amount of hours you spend practicing each time
+var danceHours = prompt("How many hours do you practice at one time?")
 
 //This is the final print to show the results.
-console.log (dancePractice)
+console.log (dancePractice + danceHours)
 
