f670cfbf4354d5cdc673501d89303a07ab37d5c6
turnerbp <turnerbp@fullsail.edu>
2013-09-11 11:16:36 -0400

    Added first prompt asking how many time you are dancing per week.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,12 @@
  * 9/10/13
  * Expressions Personal
  */
+
+// Script to find out how much exercise I am getting per month through workouts and dancing
+
+//This is the amount of times I practice my dancing
+var dancePractice = prompt("How many times per week do you practice your dancing?")
+
+//This is the final print to show the results.
+console.log (dancePractice)
+
