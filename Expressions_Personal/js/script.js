4af6f278f3a55a53366e1cb49734584e2f9dba31
turnerbp <turnerbp@fullsail.edu>
2013-09-11 11:19:38 -0400

    added prompt to see how many times per week that you run.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -10,7 +10,9 @@
 var dancePractice = prompt("How many times per week do you practice your dancing?")
 //This is the amount of hours you spend practicing each time
 var danceHours = prompt("How many hours do you practice at one time?")
+//This is to see how many times per week you run
+var runWeek = prompt("How man times do you run per week?")
 
 //This is the final print to show the results.
-console.log (dancePractice + danceHours)
+console.log (dancePractice + danceHours + runWeek)
 
