175940cf913ed944ea99b3396638cf5d8bf1778a
turnerbp <turnerbp@fullsail.edu>
2013-09-11 11:39:03 -0400

    I added the amount of tims I run per month.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -24,7 +24,9 @@ var runMinsPerWeek = runWeek *  runTime ;
 var dancePerMonth = monthWeeks * dancePractice;
 //This will calculate the amount of hours a month I practice
 var danceHrsPerMonth = danceHours * monthWeeks;
+//This will calculate the amount of Days I run per month
+var runDaysPerMonth = runWeek * monthWeeks
 
 //This is the final print to show the results.
-console.log (dancePractice + danceHours + runWeek + runTime + monthWeeks + dancePracticeHrsPerWeek + runMinsPerWeek + dancePerMonth + monthWeeks);
+console.log (dancePractice + danceHours + runWeek + runTime + monthWeeks + dancePracticeHrsPerWeek + runMinsPerWeek + dancePerMonth + danceHrsPerMonth + runDaysPerMonth);
 
