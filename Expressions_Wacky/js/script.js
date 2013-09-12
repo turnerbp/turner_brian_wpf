c8a5db3f6fe99a36a7a7da338b915a9864c1e063
turnerbp <turnerbp@fullsail.edu>
2013-09-12 11:42:15 -0400

    Added the prices to the tenor heads.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,3 +8,5 @@
 
 //this will add the price of the Standard Size Bass Drum heads (16,18,20,24,28).
 var bassDrumPrice = [30.49 + 31.49 + 33.49 + 37.95 + 46.49];
+//this will add the price of the standard size tenor drum heads (6,10,12,13,14)
+var tenorDrumPrice = [12.95 + 14.95 + 15.95 + 16.49 + 17.49];
