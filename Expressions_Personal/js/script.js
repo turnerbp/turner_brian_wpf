4d484fc5bbd55c98c799484734b1e88e781a9ba2
turnerbp <turnerbp@fullsail.edu>
2013-09-12 11:27:00 -0400

    Fixed wording.  "How many weeks in the month you are calculating?"

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -15,7 +15,7 @@ var runWeek = prompt("How man times do you run per week?");
 //This is to see how long your run for.
 var runTime = prompt("How many minutes do you run for?");
 //This is asking how many weeks in the month you are exercising in.
-var monthWeeks = prompt("How many weeks in the month you are exercising in?");
+var monthWeeks = prompt("How many weeks in the month you are calculating?");
 //This will calculate the amount of hours I practice per week.
 var dancePracticeHrsPerWeek = dancePractice * danceHours;
 //This will calculate the amount of hours you run per week.
