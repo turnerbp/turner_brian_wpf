6a5148973aa01a39afff8c066176b3af90a4db3d
turnerbp <turnerbp@fullsail.edu>
2013-09-12 11:41:36 -0400

    Created script fill.  Added Price of Bass drum heads.

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Brian Turner
+ * 9/10/13
+ * Expressions Wacky
+ */
+
+//Expressions Wacky
+
+//this will add the price of the Standard Size Bass Drum heads (16,18,20,24,28).
+var bassDrumPrice = [30.49 + 31.49 + 33.49 + 37.95 + 46.49];
