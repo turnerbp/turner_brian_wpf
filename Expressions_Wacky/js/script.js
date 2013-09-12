1523cf023f5e900ea399048d0a0fec902b88f310
turnerbp <turnerbp@fullsail.edu>
2013-09-12 11:44:24 -0400

    added the price to the snare drum heads.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -10,3 +10,5 @@
 var bassDrumPrice = [30.49 + 31.49 + 33.49 + 37.95 + 46.49];
 //this will add the price of the standard size tenor drum heads (6,10,12,13,14)
 var tenorDrumPrice = [12.95 + 14.95 + 15.95 + 16.49 + 17.49];
+//the will add the price of the standard size snare drum heads (14inch top and bottom).
+var snareDrumPrice = [34.95 + 45.95];
