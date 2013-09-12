463de0cc1c3cb89ba4645c58c00fffad21d12148
turnerbp <turnerbp@fullsail.edu>
2013-09-12 11:51:08 -0400

    Created a prompt to ask how many quads will you be marching.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -9,10 +9,12 @@
 //this will add the price of the Standard Size Bass Drum heads (16,18,20,24,28).
 var bassDrumPrice = [30.49 + 31.49 + 33.49 + 37.95 + 46.49];
 //this will add the price of the standard size tenor drum heads (6,10,12,13,14)
-var tenorDrumPrice = [12.95 + 14.95 + 15.95 + 16.49 + 17.49];
+var quadDrumPrice = [12.95 + 14.95 + 15.95 + 16.49 + 17.49];
 //the will add the price of the standard size snare drum heads (14inch top and bottom).
 var snareDrumPrice = [34.95 + 45.95];
 //creating a prompt to ask how many times you will change your drum heads in one season.
 var changeTimes = prompt("How many times will your change your drum heads in one season?");
 //creating a prompt to ask how many snare drums you will be marching.
-var snareMarch = prompt("How man snare drums will you be marching?");
+var snareMarch = prompt("How many snare drums will you be marching?");
+//creating a prompt to ask how many tenors you will be marching.
+var tenorMarch = prompt("How many quads will you be marching?");
