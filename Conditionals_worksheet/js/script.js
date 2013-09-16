b001d28d3f6f5078e78adb04354db0b9e98b230d
turnerbp <turnerbp@fullsail.edu>
2013-09-16 10:38:18 -0400

    Stuff Your Face Example 1

---
+++ Conditionals_worksheet/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Brian Turner
+ * Date: 9/15/13
+ * Conditionals Worksheet
+ */
+
+//Stuff your face I
+
+//set the weight for the heavyweight division
+var heavyWeight = 250;
+//if the pie eater is 250 or more
+if(heavyWeight >= 250){
+    //print the results below if competitor is great or equal to the set weight.
+    console.log("The competitor qualifies for the heavyweight division.");
+}else{
+    //print the results below if the competitor is less than the set weight.
+    console.log("The competitor nees to gain some weight");
+}
