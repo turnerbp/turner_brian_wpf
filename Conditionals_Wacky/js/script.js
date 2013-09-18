b0900eb4c62e0fd3f89fc9d53afe8e1a779efa92
turnerbp <turnerbp@fullsail.edu>
2013-09-18 11:08:38 -0400

    adjusted code to try and fix the results problem

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,7 +12,7 @@ var amountSuits = prompt("How many suits do you have?");
 //prompt to ask how many ties you have
 var amountTies = prompt("HOw many ties do you have?");
 //calculate the amount of suit to tie ratio
-var suitTieAmount = amountSuits - amountTies;
+var suitTieAmount = amountTies - amountSuits;
 //if or else statement to find the answer
 if(suitTieAmount <= amountSuits){
      //it it is <= amount suits
