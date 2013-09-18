99666ff5337092ed717f632161474c9e6fe3e7b7
turnerbp <turnerbp@fullsail.edu>
2013-09-18 11:06:14 -0400

    fixed code to add () were I left them out.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,17 +8,19 @@
 //Calculate how many suits you have and how many ties you need.
 
 //prompted asking how many suits you have
-var amountSuits = prompt"How many suits do you have?";
+var amountSuits = prompt("How many suits do you have?");
 //prompt to ask how many ties you have
-var amountTies = prompt"HOw many ties do you have?";
+var amountTies = prompt("HOw many ties do you have?");
 //calculate the amount of suit to tie ratio
 var suitTieAmount = amountSuits - amountTies;
 //if or else statement to find the answer
 if(suitTieAmount <= amountSuits){
      //it it is <= amount suits
-    console.log("You need to buy more ties!")
+    console.log("You need to buy more ties!");
 }else{
     //if it is greater than amountSuits
-    console.log("You have plenty of ties!")
+    console.log("You have plenty of ties!");
 }
+
+
 
