678c6603082e62d4da70cde7cfbf8480114a79b1
turnerbp <turnerbp@fullsail.edu>
2013-09-18 11:02:23 -0400

    add else if statement to determine the results.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -13,6 +13,12 @@ var amountSuits = prompt"How many suits do you have?";
 var amountTies = prompt"HOw many ties do you have?";
 //calculate the amount of suit to tie ratio
 var suitTieAmount = amountSuits - amountTies;
-
-
+//if or else statement to find the answer
+if(suitTieAmount <= amountSuits){
+     //it it is <= amount suits
+    console.log("You need to buy more ties!")
+}else{
+    //if it is greater than amountSuits
+    console.log("You have plenty of ties!")
+}
 
