40d23329353e653ca180770a30d223a6d01fd0f5
turnerbp <turnerbp@fullsail.edu>
2013-09-18 10:37:16 -0400

    Prompt to ask how many hours do you study per week

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -8,8 +8,10 @@
 //College Apartment Application
 
 //Prompt to see what the age of the person is.
-var personAge = prompt("What is your age?")
+var personAge = prompt("What is your age?");
 //Prompt to ask how many times per week do you party.
-var partyPerWeek = prompt("How many times do you party per week?")
+var partyPerWeek = prompt("How many times do you party per week?");
+//prompt to ask how many hours you study in a week
+var studyHours = prompt("How many hours do you study?");
 
 
