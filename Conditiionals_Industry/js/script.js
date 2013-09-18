4c53661683aed0be39c0bf0fe0d4daeaaf0802b3
turnerbp <turnerbp@fullsail.edu>
2013-09-18 11:24:56 -0400

    asking how many parents are going to ride the bus

--- Conditiionals_Industry/js/script.js
+++ Conditiionals_Industry/js/script.js
@@ -9,3 +9,5 @@
 
 //prompt asking how many students you have in band
 var studentsHave = prompt("How many students do you have in the Band?");
+//prompt asking how many parents are going to ride the bus
+var parentsRide = promt("How many parents are going to ride the bus?");
