8231d2509736284a42325cc90f32d43ef53042ac
turnerbp <turnerbp@fullsail.edu>
2013-09-18 10:53:16 -0400

    added prompt to ask how many suits you have?

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,5 +5,8 @@
  * Conditionals_Wacky
  */
 
+//Calculate how many suits you have and how many ties you need.
 
+//prompted asking how many suits you have
+var amountSuits = prompt"How many suits do you have?"
 
