430fe011e2eaaa74b367c2a4d341db70574e7396
turnerbp <turnerbp@fullsail.edu>
2013-09-18 12:33:17 -0400

    fixed some wording in the 2nd prompt.

--- Conditiionals_Industry/js/script.js
+++ Conditiionals_Industry/js/script.js
@@ -10,7 +10,7 @@
 //prompt asking how many students you have in band
 var studentsHave = prompt("How many students do you have in the Band?");
 //prompt asking how many parents are going to ride the bus
-var parentsRide = prompt("How many parents are going to ride the bus?");
+var parentsRide = prompt("How many parents are going to ride the Bus?");
 //prompt asking how many seats are on a bus
 var seatsBus = prompt("How many seats are on a bus?");
 //calculate the total amount of people riding the bus.
