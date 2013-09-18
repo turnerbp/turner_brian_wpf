f75ca55e8b4a9ae0d312f905e04b904bd90c8af4
turnerbp <turnerbp@fullsail.edu>
2013-09-18 10:35:33 -0400

    asked how many time you party during the week.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,6 +7,9 @@
 
 //College Apartment Application
 
-//Prompt to see what the age of the person is
+//Prompt to see what the age of the person is.
 var personAge = prompt("What is your age?")
+//Prompt to ask how many times per week do you party.
+var partyPerWeek = prompt("How many times do you party per week?")
+
 
