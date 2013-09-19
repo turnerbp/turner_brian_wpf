a00450bba0390e7e7c00c04bddf858a0b7a59016
turnerbp <turnerbp@fullsail.edu>
2013-09-19 13:13:28 -0400

    added ; to the end of the if statement

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -21,7 +21,7 @@ var shirtTieAmount = amountTies - amountShirts;
 //if/else statement added
 if(suitShirtAmount <= shirtTieAmount && suitTieAmount < amountSuits){
     //added print statement if Shirt and Tie amount are less than Suits
-   console.log("You need to buy some more Dress Shirt and Ties!")
+   console.log("You need to buy some more Dress Shirt and Ties!");
 }else{
     //added print statement to the else " Buy more Shirts and Ties"
     console.log("You do not need to buy more shirts or ties!")
