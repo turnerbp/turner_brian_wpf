ac8a85e2d692cb292634374a874e7fbc4c1e3981
turnerbp <turnerbp@fullsail.edu>
2013-09-19 12:22:14 -0400

    //Added Prompt to ask the total house budget

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -9,6 +9,7 @@
 
 //Added Prompt to ask the price of the Furniture you are going to buy.
 var furniturePrice = prompt("What is the price of the furniture you are wanting to buy?");
+//Added Prompt to ask the total house budget
 var houseBudget = prompt("What is you monthly house hold budget?");
 var monthlyIncome = prompt("What is the monthly house hold income?");
 
