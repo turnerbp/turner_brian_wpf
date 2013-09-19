6fce6875cac47e927fa9a24d27495c0012bdc76f
turnerbp <turnerbp@fullsail.edu>
2013-09-19 12:17:13 -0400

    Changed the code completely to a different calculation

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,24 +4,3 @@
  * Assignment Conditionals
  * Conditionals Personal
  */
-
-//College Apartment Application
-
-//Prompt to see what the age of the person is.
-var personAge = prompt("What is your age?");
-//Prompt to ask how many times per week do you party.
-var partyPerWeek = prompt("How many times do you party per week?");
-//prompt to ask how many hours you study in a week
-var studyHours = prompt("How many hours do you study?");
-//minimum age required to be in apartment
-var apartmentAge = 20
-//minimum hours of stud required to be in apartment
-var minimumStudy = 25
-//maximum party hours per week
-var maximumParty = 10
-
-
-
-
-
-
