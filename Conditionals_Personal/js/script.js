717d170f6ba42e81094be5fb3fc60be58b620916
turnerbp <turnerbp@fullsail.edu>
2013-09-19 12:21:33 -0400

    Added Prompt to ask the price of the Furniture you are going to buy.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,3 +4,11 @@
  * Assignment Conditionals
  * Conditionals Personal
  */
+
+//Buy new Furniture
+
+//Added Prompt to ask the price of the Furniture you are going to buy.
+var furniturePrice = prompt("What is the price of the furniture you are wanting to buy?");
+var houseBudget = prompt("What is you monthly house hold budget?");
+var monthlyIncome = prompt("What is the monthly house hold income?");
+
