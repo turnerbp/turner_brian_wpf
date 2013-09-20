27bacaadfa8f0bf3efdc259a8094641d1b1de06f
turnerbp <turnerbp@fullsail.edu>
2013-09-19 20:41:13 -0400

    check to see if calculation will work

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -21,5 +21,5 @@ var moneyLeft = monthlyIncome - houseBudget;
 var moneyNeeded = furniturePrice - moneyLeft;
 //calculation to see how much you need to save per month
 var savePerMonth = furniturePrice / monthSave;
-console.log(moneyNeeded);
+console.log(savePerMonth);
 
