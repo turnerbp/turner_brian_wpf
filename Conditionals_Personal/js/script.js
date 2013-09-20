3a96d75778703ccea15ab47ad0963e37b5b71d5a
turnerbp <turnerbp@fullsail.edu>
2013-09-19 20:25:47 -0400

    added prompt to  ask the price of the furniture

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,7 +5,10 @@
  * Conditionals Personal
  */
 
+//Buy new Furniture
 
+//Added Prompt to ask the price of the Furniture you are going to buy.
+var furniturePrice = prompt("What is the price of the furniture you are wanting to buy?");
 
 
 
