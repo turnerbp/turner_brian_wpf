5b3752f56081cef1f1d708366c092a703225f745
turnerbp <turnerbp@fullsail.edu>
2013-09-19 20:36:59 -0400

    added prompt to ask how many months you want to save

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -13,9 +13,12 @@ var furniturePrice = prompt("What is the price of the furniture you are wanting 
 var houseBudget = prompt("What is you monthly house hold budget?");
 //Added Prompt to ask the Monthly Income.
 var monthlyIncome = prompt("What is the monthly house hold income?");
+//Added prompt to ask how many months you want to save money
+var monthSave = prompt("How many months do you want to save?")
 //calculating the amount of money you have after you pay your bills
 var moneyLeft = monthlyIncome - houseBudget;
 //calculate money left over minus the furniture price to see how much left you need to buy the furniture
 var moneyNeeded = furniturePrice - moneyLeft;
+
 console.log(moneyNeeded);
 
