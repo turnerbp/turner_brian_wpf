8ffaa59f7e391014beb37297a0d5ac7768705004
turnerbp <turnerbp@fullsail.edu>
2013-09-19 20:24:58 -0400

    setting up js file...

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -5,12 +5,7 @@
  * Conditionals Personal
  */
 
-//Buy new Furniture
 
-//Added Prompt to ask the price of the Furniture you are going to buy.
-var furniturePrice = prompt("What is the price of the furniture you are wanting to buy?");
-//Added Prompt to ask the total House Budget.
-var houseBudget = prompt("What is you monthly house hold budget?");
-//Added Prompt to ask the Monthly Income.
-var monthlyIncome = prompt("What is the monthly house hold income?");
+
+
 
