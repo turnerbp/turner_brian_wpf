5753d70c02e4c250de9f1b30bf9cf23ab07966a7
turnerbp <turnerbp@fullsail.edu>
2013-09-26 23:47:02 -0400

    added comments to the promt asking What is the length of your warm-ups in mins

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -8,6 +8,7 @@
 
 //prompt asking how many time do you want to play your warm-ups
 var warmupTime = prompt("How many time do you want to play your warm-ups?");
+//prompt asking What is the length of your warm-ups in mins
 var warmupLength = prompt("What is the length of your warm-ups in mins?");
 var warmUpTotal = prompt("How many warm-ups do you have?");
 var calcTime = function(totalTimeEach, totalWarm){
