d60dee62bc71e682bdc483fa31944558d82971e3
turnerbp <turnerbp@fullsail.edu>
2013-09-26 23:40:07 -0400

    added prompt asking how many time do you want to play your warm-ups

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -9,3 +9,5 @@
 //prompt asking how many time do you want to play your warm-ups
 var warmupTime = prompt("How many time do you want to play your warm-ups?");
 var warmupLength = prompt("What is the length of your warm-ups in mins?");
+var warmUpTotal = prompt("How many warm-ups do you have?");
+
