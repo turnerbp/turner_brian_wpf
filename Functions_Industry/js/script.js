0640f0b369169ff1676cf06252811cc7e68c52c4
turnerbp <turnerbp@fullsail.edu>
2013-09-26 21:12:10 -0400

    setting up js file.  Also added prompt to ask how many vibes you are marching

---
+++ Functions_Industry/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Brian Turner
+ * 9/26/13
+ * Assignment Functions
+ */
+
+//Functions Industry
+
+//Prompt asking how many Vibes you are marching
+var vibesMarch = prompt("How many vibes are you marching?");
+
