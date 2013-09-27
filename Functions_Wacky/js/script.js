8b130e865fcf094307152b39e1cca09be787a58d
turnerbp <turnerbp@fullsail.edu>
2013-09-26 23:40:57 -0400

    added calculation to the function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -10,4 +10,8 @@
 var warmupTime = prompt("How many time do you want to play your warm-ups?");
 var warmupLength = prompt("What is the length of your warm-ups in mins?");
 var warmUpTotal = prompt("How many warm-ups do you have?");
+var calcTime = function(totalTimeEach, totalWarm){
+    var time = totalTimeEach * totalWarm * warmupTime;
+    return time;
+}
 
