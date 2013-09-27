0777a1821efbac9e6ce57f80bf06722e009139a7
turnerbp <turnerbp@fullsail.edu>
2013-09-26 21:13:28 -0400

    prompt asking how many marimbas are you marching...

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,4 +8,5 @@
 
 //Prompt asking how many Vibes you are marching
 var vibesMarch = prompt("How many vibes are you marching?");
-
+//Prompt asking how many Marimbas you are marching
+var marimbasMarch = prompt("How many marimbas are you marching?");
